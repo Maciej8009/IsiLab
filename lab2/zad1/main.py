895ce9a9626f48dc0b219ae935b4e7139034f891
Maciej Kowalik <100430330+Maciej8009@users.noreply.github.com>
2024-03-26 00:59:22 +0100

    Delete lab2 directory

--- lab2/zad1/main.py
+++
@@ -1,5 +0,0 @@
-import Dog
-
-
-dog1 = Dog("Bob", 20, "male", "male")
-dog1.sound()
--- lab2/zad6/Home.py
+++
@@ -1,5 +0,0 @@
-class Home:
-    def __init__(self, header_name, price, price_for_m2):
-        self.header_name = header_name
-        self.price = price
-        self.price_for_m2 = price_for_m2
